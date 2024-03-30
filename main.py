18a668bcf76ccf180a25b1a17c45dc4cb31d2b81
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-30 11:03:38 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from tkinter import *
+from tkinter.ttk import Combobox
+import random
 root = Tk()
 root.geometry("400x300")
 root.title("Генератор паролей")
-root.mainloop()
+root.configure(background ="bisque")
+def gen():
+    global sc1
+    sc1.set("")
+    passw=("")
+    length=int(sc1.get())
